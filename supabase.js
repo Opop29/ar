95cedf8e45f44299823d0b51d31e138c3fa7622f
Joshua tesoro Quidit <133975934+Opop29@users.noreply.github.com>
2025-08-25 19:25:42 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+<script type="module">
+  import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+  export const supabase = createClient(
+    "https://clywxultbpgqlqlorjjh.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNseXd4dWx0YnBncWxxbG9yampoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMjAzNzUsImV4cCI6MjA3MTY5NjM3NX0.nAAU2oT9-hy7tekZGsVwNMKzTzZSRCvJWjBEiba8F3U"
+  );
+</script>
